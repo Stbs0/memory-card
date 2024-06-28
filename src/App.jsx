7568b975296535119f27be15742f87487966cb0b
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-06-28 22:44:49 +0300

    add background to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-
+import BackgroundImg from "./assets/bg.jpg";
 import { Pokedex } from "pokeapi-js-wrapper";
 
 function App() {
   const [count, setCount] = useState(0);
-  const P = new Pokedex();
+
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div className='h-screen relative *:text-white '>
+      <img
+        src={BackgroundImg}
+        alt=''
+        className=' absolute  w-full object-center object-cover h-full opacity-70 -z-10'
+      />
+      <div className='h-16  bg-slate-700 rounded-b-xl'>hello</div>
+    </div>
   );
 }
 
