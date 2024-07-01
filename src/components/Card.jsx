23448ae2770ec42408fb361d490958a99b14d86e
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-07-01 14:24:25 +0300

    feat: display pictures and names of pokemone

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
-function Card() {
+function Card({ img, name }) {
   return (
     <div className=' flex flex-col items-center bg-slate-400/40 rounded-lg w-fit px-2 pt-2  space-y-2'>
       <img
         className='size-40 rounded-md'
-        src='src\assets\bg.jpg'
+        src={img}
         alt=''
       />
 
-      <p>image</p>
+      <p>{name}</p>
     </div>
   );
 }
--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,32 +1,37 @@
 import Card from "./Card";
 import axios from "axios";
 import { useEffect, useState } from "react";
-function GamePlay() {
+import shuffle from "../utilis/shuffleCards";
+// TODO: add status to keep score
+function GamePlay({count}) {
   const [cards, setCards] = useState([]);
+  
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=30").then((res) => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=15").then((res) => {
       const picsAndNames = res.data.results.map((pokemon) => {
         pokemon.pic = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
           pokemon.url.split("/")[6]
         }.svg`;
-        delete pokemon.url
+        delete pokemon.url;
         return pokemon;
       });
       setCards(picsAndNames);
     });
   }, []);
-  console.log(cards)
+  console.log(cards);
+  const shuffledCards = shuffle([...cards])
   return (
     <div className='bg-slate-500/70   backdrop-blur-sm  container  mx-auto  rounded-lg text-white    px-5  py-4  '>
       <div className=' flex flex-wrap justify-center   gap-7 '>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {shuffledCards.map((card) => {
+          return (
+            <Card
+              key={card.name}
+              img={card.pic}
+              name={card.name}
+            />
+          );
+        })}
       </div>
     </div>
   );
